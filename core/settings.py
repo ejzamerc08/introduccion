62cabbfc411f9692dd666b2f7a3bd2043173f10f
ejzamerc08 <erickz16@hotmail.com>
2022-07-02 11:46:38 -0500

    creamos la app blog y mostramos la view con el archivo html

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'core',
+    'blog',
 ]
 
 MIDDLEWARE = [
